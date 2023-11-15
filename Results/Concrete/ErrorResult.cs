ae427adc0b29dbc697b3d313e4b971acacede71c
Mirze123 <mirze0777@gmail.com>
2023-11-15 17:54:17 +0400

    Add project files.

---
+++ Results/Concrete/ErrorResult.cs
@@ -0,0 +1,14 @@
+﻿namespace LibraryManagement;
+public class ErrorResult : Result
+{
+    public ErrorResult() : base(false)
+    {
+
+    }
+
+    public ErrorResult(string message) : base(false, message)
+    {
+
+    }
+}
+
